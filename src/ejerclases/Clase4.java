3b12c992473b1f593d52be5fe78b362d3260a975
Carlos Vazquez <cgvr.carlos@gmail.com>
2022-12-28 20:56:04 -0600

    agregando solucion clase4

---
+++ src/ejerclases/Clase4.java
@@ -0,0 +1,11 @@
+package ejerclases;
+
+public class Clase4 {
+	public static void main (String[] args) {
+		double c = 20.0;
+		double f;
+		f = ((double) 9/5) * c + 32.0;
+		
+		System.out.println(f);
+	}
+}
