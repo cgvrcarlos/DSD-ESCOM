4334df3ad3531ebef748ec773e0c3c7a82b272fb
Carlos Vazquez <cgvr.carlos@gmail.com>
2022-12-28 23:58:54 -0600

    agregando solucion clase 5 ejercicio 2

---
+++ src/ejerclases/Clase52.java
@@ -0,0 +1,49 @@
+package ejerclases;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Clase52 {
+	public static void main(String[] args) {
+		int total = 0;
+
+		int n_tokens = Integer.parseInt(args[0]);
+
+		String token = "IPN";
+
+		StringBuilder cadena = llenarArreglo(n_tokens);
+
+		total = encontrarCadena(token, cadena, n_tokens);
+
+		System.err.println("Se encontraron " + total + " coincidencias de IPN");
+	}
+
+	public static int encontrarCadena(String p, StringBuilder cad, int n) {
+        int total = 0;
+        int flag = 0;
+        
+		while ((flag = cad.indexOf(p, flag)) != -1){
+            flag += 4;
+            total++;
+        }
+
+        return total;
+    }
+
+	public static StringBuilder llenarArreglo(int n) {
+        StringBuilder cad = new StringBuilder();
+        for (int i = 0, j = 0; i < (n * 4); i++) {
+            if (j == 3) {
+                cad.append(" ");
+                j = 0;
+            }else {
+                cad.append(generaLetra());
+                j++;
+            }
+        }
+        return cad;
+    }
+
+	public static char generaLetra() {
+        return (char) ThreadLocalRandom.current().nextInt(65, 90 + 1);
+    }
+}
